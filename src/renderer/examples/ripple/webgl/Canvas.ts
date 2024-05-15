41f739b8d0c391e7a5acab2e7f0fc01357156580
Akifumi Nakamura <a-nakamura@studio-spoon.co.jp>
2024-05-16 01:37:29 +0900

    rippleサンプルのアレンジ

--- src/renderer/examples/ripple/webgl/Canvas.ts
+++ src/renderer/examples/ripple/webgl/Canvas.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { RawShaderMaterial } from '@scripts/webgl/core/ExtendedMaterials'
 import { Three } from '@scripts/webgl/core/Three'
 import * as THREE from 'three'
@@ -17,22 +18,45 @@ export class Canvas extends Three {
     this.simulator = new Simulator(this.renderer)
     this.fps = new Fps()
 
-    this.loadTexture().then((texture) => {
-      this.screen = this.createScreen(texture)
+    this.loadVideoTexture().then(({ texture, aspect }) => {
+      this.screen = this.createScreen(texture, aspect)
       window.addEventListener('resize', this.resize.bind(this))
+      this.resize()
       this.startRendering()
     })
   }
 
-  private async loadTexture() {
-    const loader = new THREE.TextureLoader()
-    // https://vitejs.dev/guide/assets#new-url-url-import-meta-url
-    const texture = await loader.loadAsync(new URL('/assets/images/color_checker.webp', import.meta.url).href)
-    texture.userData.aspect = texture.source.data.width / texture.source.data.height
-    return texture
+  // private async loadTexture() {
+  //   const loader = new THREE.TextureLoader()
+  //   // https://vitejs.dev/guide/assets#new-url-url-import-meta-url
+  //   const texture = await loader.loadAsync(
+  //     new URL('/assets/images/color_checker.webp', import.meta.url).href
+  //   )
+  //   texture.userData.aspect = texture.source.data.width / texture.source.data.height
+  //   return texture
+  // }
+
+  private async loadVideoTexture() {
+    const video = document.createElement('video')
+    video.src = new URL('../videos/sea.mp4', import.meta.url).href
+    video.loop = true
+    video.muted = true
+    video.play()
+
+    await new Promise<void>((resolve) => {
+      video.onloadeddata = () => resolve()
+    })
+
+    const texture = new THREE.VideoTexture(video)
+    texture.needsUpdate = true
+
+    return {
+      texture,
+      aspect: video.videoWidth / video.videoHeight
+    }
   }
 
-  private createScreen(texture: THREE.Texture) {
+  private createScreen(texture: THREE.Texture, aspect: number) {
     const dpr = this.renderer.getPixelRatio()
 
     const geo = new THREE.PlaneGeometry(2, 2)
@@ -40,12 +64,12 @@ export class Canvas extends Three {
       uniforms: {
         uSim: { value: null },
         uImage: { value: texture },
-        uCoveredScale: { value: this.coveredScale(texture.userData.aspect) },
-        uResolution: { value: [this.size.width * dpr, this.size.height * dpr] },
+        uCoveredScale: { value: this.coveredScale(aspect) },
+        uResolution: { value: [this.size.width * dpr, this.size.height * dpr] }
       },
       vertexShader: screenVs,
       fragmentShader: screenFs,
-      glslVersion: '300 es',
+      glslVersion: '300 es'
     })
     const mesh = new THREE.Mesh(geo, mat)
     this.scene.add(mesh)
@@ -57,7 +81,9 @@ export class Canvas extends Three {
 
     const dpr = this.renderer.getPixelRatio()
     const uniforms = this.screen.material.uniforms
-    uniforms.uCoveredScale.value = this.coveredScale(uniforms.uImage.value.userData.aspect)
+    uniforms.uCoveredScale.value = this.coveredScale(
+      uniforms.uImage.value.image.videoWidth / uniforms.uImage.value.image.videoHeight
+    )
     uniforms.uResolution.value = [this.size.width * dpr, this.size.height * dpr]
   }
 
